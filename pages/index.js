6e1f8591a98f38eb7ddb7f64649cc2114c93100a
X-Wallet-5D <xwallet5d@gmail.com>
2025-10-30 13:58:54 -0300

    Primeira página

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <Contador />
+    </div>
+  );
+}
+
+function Contador() {
+  const [contador, setContador] = useState(1);
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={() => setContador(contador + 1)}>Adicionar</button>
+    </div>
+  );
+}
+
+export default Home;
